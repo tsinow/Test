1c21b984cedef89f8a1ccdcb48a6fbf88e07f210
lqw <2020092039@email.szu.edu.cn>
2022-11-16 18:33:36 +0800

    file initialize

---
+++ cmake-build-debug/CMakeFiles/1000.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/1000jiechen/1000jiechen/StdAfx.cpp" "/mnt/d/1000jiechen/cmake-build-debug/CMakeFiles/1000.dir/1000jiechen/StdAfx.cpp.o"
+  "/mnt/d/1000jiechen/1000jiechen/jiechen.cpp" "/mnt/d/1000jiechen/cmake-build-debug/CMakeFiles/1000.dir/1000jiechen/jiechen.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../1000jiechen"
+  "../1000jiechen/Debug"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
